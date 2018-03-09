4bfafbdf9e9e348ba8eac83123f3f7c601148d54
ssaegert <ssaegert@gmail.com>
2018-03-09 14:48:33 -0600

    working on conflict

--- src/Author.java
+++ src/Author.java
@@ -1,36 +1,36 @@
 /**
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
      * @deprecated Use publishedBooks instead
      */
-    @SuppressWarnings("unchecked")
     @Deprecated
     public List<String> getBooks() {
         return books;
     }
-    @SuppressWarnings("unchecked")
+
     public List<String> publishedBooks() {
         return books;
     }
-    @SuppressWarnings("unchecked")
+
     public void addBook(String book) {
         books.add(book);
     }
 
-//    @Override
-    public String sortName() {
+    @Override
+    public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
